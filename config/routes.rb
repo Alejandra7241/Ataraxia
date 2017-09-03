af2151d6cb390adaaeff2fbdb36bbc2199e39a35
ltenganah <ltenganah@unal.edu.co>
2017-09-03 06:06:02 +0000

    landing page and contact form added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  #devise_for :admins
   devise_for :users
   #devise_for :users, controllers: { sessions: 'users/sessions' }
   #resources :usuarios, as: :users, only: [ :index ]
   root 'welcome#index'
+
+  
+  resources :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
