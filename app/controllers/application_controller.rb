82eb17afb58319043a76201f737a60e3f9fb3336
Nicolás Restrepo Torres <nrestrepot@unal.edu.co>
2017-09-05 20:13:31 -0500

    Testing 404 in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
   protect_from_forgery with: :exception
   #Descomentar esta linea si se requiere el login
   #before_action :authenticate_user!
