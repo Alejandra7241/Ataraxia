4f3d2d16b2843676be58e322086a91cbca78bbb0
Nicolás Restrepo Torres <nrestrepot@unal.edu.co>
2017-09-18 02:39:10 -0500

    Processing academic history

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -14,7 +14,11 @@ class StudentController < ApplicationController
     checking_notes = false
     hap = Array.new
     arr = {}
+    creditos_totales = 0
+    creditos_totales_pa = 0
     current_semester = 1
+    ponderacion = 0.0
+    ponderacion_pa = 0.0
     informacion.each_line do |line|
         
         if line =~ /\d/
@@ -44,6 +48,14 @@ class StudentController < ApplicationController
                 codigo_actual = processing[0].split('-')
                 codigo_actual = codigo_actual[0]
                 nota = Float(processing[-1]) rescue processing[-1].upcase
+                flag = Float(nota) rescue false
+                if flag
+                    creditos = processing[-3].to_f
+                    creditos_totales += creditos
+                    ponderacion += creditos*nota
+                    creditos_totales_pa += creditos unless nota.to_i < 3
+                    ponderacion_pa += creditos*nota unless nota.to_i < 3
+                end
                 arr[codigo_actual.to_i] = nota
                 puts "El codigo de la materia es #{codigo_actual.to_i} y la nota es #{nota} y el semestre es #{current_semester}"
             end
@@ -59,9 +71,17 @@ class StudentController < ApplicationController
         current_semester += 1
     end
     print hap
+    papa = ponderacion/creditos_totales.to_f
+    pa = ponderacion_pa/creditos_totales_pa.to_f
+    puts ""
+    puts "El PAPA es #{papa} y el Promedio Academico es #{pa}"
     #puts informacion[\d*\s*[|]\s*[a-z]*\s*[a-z]*\s*[a-z]*\s*[a-z]\s*[a-z]*$]
     puts "///////////////////////////////////////////////////////////////////////////////////////////"
     #redirect_to root_path
-    puts @history
+    @pa = pa
+    @papa = papa
+    @hap = hap
+    puts "senid to view"
+    @current_semester = 1
     end
 end
