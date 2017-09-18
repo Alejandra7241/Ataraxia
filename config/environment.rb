1f59a1fb3f3aebbd7300ec6098a18327ec15b6b1
Nicolás Restrepo Torres <nrestrepot@unal.edu.co>
2017-09-17 20:17:16 -0500

    Host for heroku again

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-#Override for deploying in Heroku
-config.action_mailer.default_url_options = { :host => 'localhost' }
