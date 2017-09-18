13134a8ca69809285a77bfb8719ca824b761cbf4
Nicolás Restrepo Torres <nrestrepot@unal.edu.co>
2017-09-17 20:02:12 -0500

    Heroku host link added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Override for deploying in Heroku
+config.action_mailer.default_url_options = { :host => 'localhost' }
