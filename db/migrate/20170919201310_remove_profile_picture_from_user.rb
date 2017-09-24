984033df3b6049900d2aef1f88f7b37a8f5dc68b
bsgarciac <nrestrepot@unal.edu.co>
2017-09-24 19:42:03 +0000

    Added conditional to migration for Heroku deployment

--- db/migrate/20170919201310_remove_profile_picture_from_user.rb
+++ db/migrate/20170919201310_remove_profile_picture_from_user.rb
@@ -1,5 +1,5 @@
 class RemoveProfilePictureFromUser < ActiveRecord::Migration[5.1]
   def change
-    remove_column :users, :profile_picture, :string column_exists? :users, :profile_picture
+    remove_column :users, :profile_picture, :string if column_exists? :users, :profile_picture
   end
 end
