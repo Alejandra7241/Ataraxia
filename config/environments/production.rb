375a286aa9324e75b886029018bd9513da094ad2
Nicolas Restrepo Torres <nrestrepot@unal.edu.co>
2017-10-05 01:04:21 +0000

    Hiding variables for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
   # Setting environment variables for development
-  
+  SECRET_KEY_BASE = ENV['secret_key_base']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
