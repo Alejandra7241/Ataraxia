35a2f63728db6d3a68dd1febd6e752238d687622
Lizzy <ltenganah@unal.edu.co>
2017-10-26 22:04:58 +0000

    db recreated again

--- db/migrate/20171019193823_add_admin_to_malla.rb
+++ db/migrate/20171019193823_add_admin_to_malla.rb
@@ -1,5 +1,5 @@
 class AddAdminToMalla < ActiveRecord::Migration[5.1]
   def change
-    add_reference(:mallas, :admin, references: :users, index: true, foreign_key: true)
+    add_reference(:mallas, :admin, foreign_key: {to_table: :users}, index: true)
   end
 end
