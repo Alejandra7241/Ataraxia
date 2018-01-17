762faaebfdb3663e8abfbf3e06cf8f157a4295d5
Brayan <bsgarciac@unal.edu.co>
2018-01-17 22:14:05 +0000

    Malla ingenieria mecanica y cambios en los seeds

---
+++ db/seeds_s/biologia.rb
@@ -0,0 +1,256 @@
+#-------------------------------------------------------------------------------------------
+
+#---------- Malla estándar Biología ----------#
+
+career = Career.create({code: 2513, name: 'Biología', department: 'Departamento de Biología', faculty: 'Ciencias'})
+
+malla = Malla.create({tipo:'Estándar', career_id: career.id})
+
+malla.semesters.create([{:number => 1}, {:number => 2}, {:number => 3}, {:number => 4}, {:number => 5}, {:number => 6}, {:number => 7}, {:number => 8} , {:number => 9}, {:number => 10}])
+
+# Nivelación
+subj = Subject.find_by(code: 1000002)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000001)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000044)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000045)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000046)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000047)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000052)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000053)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000074)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+# Semestre 1
+
+sem = malla.semesters.find_by number: 1
+#Ecología
+subj = Subject.find_by(code: 2017777)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Ciencias de la tierra
+subj = Subject.find_by(code: 2017776)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Principios de química
+subj = Subject.find_by(code: 1000024)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Laboratorio tecnicas básicas en química
+subj = Subject.find_by(code: 1000025)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+        
+# Semestre 2
+
+sem = malla.semesters.find_by number: 2
+        
+#Biología de plantas
+subj = Subject.find_by(code: 2015877)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Biología evolutiva
+subj = Subject.find_by(code: 2017774)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Principios de química orgánica
+subj = Subject.find_by(code: 1000030)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Laboratorio principios de química orgánica
+subj = Subject.find_by(code: 1000031)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+        
+# Semestre 3
+
+sem = malla.semesters.find_by number: 3
+
+#Biología animal
+subj = Subject.find_by(code: 2017772)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Biología de microorganismos
+subj = Subject.find_by(code: 2017773)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Principios de bioquímica
+subj = Subject.find_by(code: 2023214)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Laboratorio principios de bioquímica
+subj = Subject.find_by(code: 2017010)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+# Semestre 4
+
+sem = malla.semesters.find_by number: 4
+
+#Biologia molecular de la célula
+subj = Subject.find_by(code: 2017775)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Fundamentos de investigación
+subj = Subject.find_by(code: 2017778)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Cálculo diferencial
+subj = Subject.find_by(code: 1000004)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Mecánica y ondas para biociencias
+subj = Subject.find_by(code: 1000023)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+        
+# Semestre 5
+
+sem = malla.semesters.find_by number: 5
+
+#Genética
+subj = Subject.find_by(code: 2017779)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Bioestadística fundamental
+subj = Subject.find_by(code: 1000012)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+        
+# Semestre 6
+
+sem = malla.semesters.find_by number: 6
+
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+
+        
+# Semestre 7
+
+sem = malla.semesters.find_by number: 7
+
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+
+# Semestre 8
+
+sem = malla.semesters.find_by number: 8
+
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+# Semestre 9
+
+sem = malla.semesters.find_by number: 9
+
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+# Semestre 10
+
+sem = malla.semesters.find_by number: 10
+
+#Trabajo de Grado
+subj = Subject.find_by(code: 2023028)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+
+
+
---
+++ db/seeds_s/ingenieria_mecatronica.rb
@@ -0,0 +1,330 @@
+#-------------------------------------------------------------------------------------------
+
+        #---------- Malla estándar Ingeniería Mecatrónica ----------#
+
+career = Career.create({code: 2548, name: 'Ingeniería Mecatrónica', department: 'Departamento de Ingeniería Mecanica y Mecatrónica', faculty: 'Ingeniería'})
+
+mallaIEnica = Malla.create({tipo:'Estándar', career_id: career.id})
+
+mallaIEnica.semesters.create([{:number => 1}, {:number => 2}, {:number => 3}, {:number => 4}, {:number => 5}, {:number => 6}, {:number => 7}, {:number => 8}, {:number => 9}, {:number => 10}])
+
+# Nivelación
+subj = Subject.find_by(code: 1000002)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000001)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000044)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000045)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000046)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000047)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000052)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000053)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+subj = Subject.find_by(code: 1000074)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
+career.career_has_subjects << chs
+
+
+# Semestre 1
+
+sem = mallaIEnica.semesters.find_by number: 1
+#Cálculo Diferencial
+subj = Subject.find_by(code: 1000004)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Programacion de computadores
+subj = Subject.find_by(code: 2015734)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Dibujo básico
+subj = Subject.find_by(code: 2015711)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Tecnologia Mecánica Básica
+subj = Subject.find_by(code: 2017278)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Ingeniería y Desarrollo Sostenible
+subj = Subject.find_by(code: 2017265)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+# Semestre 2
+
+sem = mallaIEnica.semesters.find_by number: 2
+
+#Calculo integral
+subj = Subject.find_by(code: 1000005)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Algebra lineal
+subj = Subject.find_by(code: 1000003)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Fundamentos de mecanica 
+subj = Subject.find_by(code: 1000019)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Taller de Ingeniería Electrónica
+subj = Subject.find_by(code: 2016509)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+# Semestre 3
+
+sem = mallaIEnica.semesters.find_by number: 3
+#Cálculo en varias variables
+subj = Subject.find_by(code: 1000006)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Probabilidad y estadistica fundamental
+subj = Subject.find_by(code: 1000013)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Fundamentos de electricidad y magnetismos
+subj = Subject.find_by(code: 1000017)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Principios de química
+subj = Subject.find_by(code: 1000024)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Principios de Estática
+subj = Subject.find_by(code: 2016640)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Circuitos Eléctricos I
+subj = Subject.find_by(code: 2016489)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+
+#Semestre 4
+
+sem = mallaIEnica.semesters.find_by number: 4
+#Ecuaciones Diferenciales
+subj = Subject.find_by(code: 1000007)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Principios de dinámica
+subj = Subject.find_by(code: 2017271)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Programación orientada a objetos
+subj = Subject.find_by(code: 2016375)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electrónica Análoga I
+subj = Subject.find_by(code: 2016495)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+
+        
+#Semestre 5
+
+sem = mallaIEnica.semesters.find_by number: 5
+#Variable compleja
+subj = Subject.find_by(code: 2015159)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Resistencia de Materiales
+subj = Subject.find_by(code: 2017277)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Ciencia e Ingeniería de Materiales
+subj = Subject.find_by(code: 2017256)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Estructuras de Datos
+subj = Subject.find_by(code: 2016699)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Señales y Sistemas I
+subj = Subject.find_by(code: 2016506)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+#Semestre 6
+
+sem = mallaIEnica.semesters.find_by number: 6
+#Ingenieria economica
+subj = Subject.find_by(code: 2015703)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Diseño Mecatrónico
+subj = Subject.find_by(code: 2017282)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electrónica Digital I
+subj = Subject.find_by(code: 2016498)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Señales y Sistemas II
+subj = Subject.find_by(code: 2016507)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#TPI
+subj = Subject.find_by(code: 2024045)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+#Semestre 7
+
+sem = mallaIEnica.semesters.find_by number: 7
+#Redes de computadores
+subj = Subject.find_by(code: 2025967)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Microcontroladores
+subj = Subject.find_by(code: 2016753)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Control
+subj = Subject.find_by(code: 2016493)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Sensores y Actuadores
+subj = Subject.find_by(code: 2017287)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+
+
+#Semestre 8
+
+sem = mallaIEnica.semesters.find_by number: 8
+#Servomecanismos
+subj = Subject.find_by(code: 2017288)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Robótica
+subj = Subject.find_by(code: 2016770)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+
+#Semestre 9
+
+sem = mallaIEnica.semesters.find_by number: 9
+#Automatización de Procesos de Manufactura
+subj = Subject.find_by(code: 2017280)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Proyecto Aplicado de Ingeniería
+subj = Subject.find_by(code: 2017275)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666664)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666664)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
+        
+# Semestre 10
+sem = mallaIEnica.semesters.find_by number: 10
+#Trabajo de Grado
+subj = Subject.find_by(code: 2017297)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666664)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666663)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+
