6244082748d48eeee9097650aaef90722f4825fc
Brayan <bsgarciac@unal.edu.co>
2018-01-23 01:40:34 +0000

    Malla Ciencias de la computación

--- db/seeds_completos/a_subjects.rb
+++ db/seeds_completos/a_subjects.rb
@@ -502,6 +502,20 @@ Subject.create([
         {code: 2017779, name: 'Genética', credits: 4},
         {code: 2023028, name: 'Trabajo de Grado', credits: 8},
         
+        #Ciencias de la computación
+        {code: 2026573, name: 'Introducción a las ciencias de la computación y a la programación', credits: 3},
+        {code: 2015181, name: 'Sistemas numéricos', credits: 4},
+        {code: 2025819, name: 'Introducción a la teoría de conjuntos', credits: 4},
+        {code: 2026519, name: 'Ecuaciones en diferencias finitas y sistemas dinámicos', credits: 4},
+        {code: 2015155, name: 'Introducción al análisis real', credits: 4},
+        {code: 2026555, name: 'Algebra abstracta y computacional', credits: 4},
+        {code: 2027312, name: 'Introducción al análisis combinatorio', credits: 4},
+        {code: 2015184, name: 'Teoría de grafos', credits: 4},
+        {code: 2015150, name: 'Análisis numérico', credits: 4},
+        {code: 2027313, name: 'Teoría de Codificación', credits: 4},
+        {code: 2027629, name: 'Lógica computacional', credits: 4},
+        {code: 9992933, name: 'Trabajo de Grado', credits: 8},
+
         # -------------------------------------- MEDICINA ------------------------------------
         
         #Medicina
--- db/seeds_completos/ciencias_de_la_computacion.rb
+++ db/seeds_completos/ciencias_de_la_computacion.rb
@@ -1,12 +1,12 @@
-#-------------------------------------------------------------------------------------------
 
-#---------- Malla estándar Psicologia ----------#
+#---------- Malla estándar Ciencias de la computación ----------#
 
-career = Career.create({code: 2535, name: 'Psicología', department: 'Departamento de Psicología', faculty: 'Ciencias Humanas'})
+career = Career.create({code: 2933, name: 'Ciencias de la computación', department: 'Departamento de Ciencias de la computación', faculty: 'Ciencias'})
 
 malla = Malla.create({tipo:'Estándar', career_id: career.id})
 
-malla.semesters.create([{:number => 1}, {:number => 2}, {:number => 3}, {:number => 4}, {:number => 5}, {:number => 6}, {:number => 7}, {:number => 8}, {:number => 9}, {:number => 10}])
+malla.semesters.create([{:number => 1}, {:number => 2}, {:number => 3}, {:number => 4}, {:number => 5}, {:number => 6}, {:number => 7}, {:number => 8}, {:number => 9}])
+
 
 # Nivelación
 subj = Subject.find_by(code: 1000002)
@@ -46,224 +46,214 @@ chs = CareerHasSubject.create( :subject => subj, :typology => 'P')
 career.career_has_subjects << chs
 
 # Semestre 1
-
 sem = malla.semesters.find_by number: 1
 
-#Fundamentos de psicología
-subj = Subject.find_by(code: 2017881)
+#Fundamentos de matemáticas
+subj = Subject.find_by(code: 2015168)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Biología general
-subj = Subject.find_by(code: 1000009)
+#Cálculo diferencial en una variable
+subj = Subject.find_by(code: 2016377)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Problemática Social Colombiana
-subj = Subject.find_by(code: 2021668)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs
-#Epistemología de las ciencias sociales
-subj = Subject.find_by(code: 2017875)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs
-#Electiva
+#Electiva 
 subj = Subject.find_by(code: 6666663)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
 career.career_has_subjects << chs
-sem.career_has_subjects << chs        
+sem.career_has_subjects << chs
+#Introducción a las ciencias de la computación y a la programación
+subj = Subject.find_by(code: 2026573)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
 
 
-
+        
 # Semestre 2
-
 sem = malla.semesters.find_by number: 2
-        
-#Estadística social fundamental
-subj = Subject.find_by(code: 1000014)
+
+#Álgebra lineal básica
+subj = Subject.find_by(code: 2015555)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Bases Biológicas del Comportamiento
-subj = Subject.find_by(code: 2017857)
+#Cálculo integral en una variable
+subj = Subject.find_by(code: 2015556)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Epistemología de las ciencias naturales
-subj = Subject.find_by(code: 2017874)
+#Sistemas numéricos
+subj = Subject.find_by(code: 2015181)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Bases Socio-culturales del Comportamiento Humano
-subj = Subject.find_by(code: 2017858)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+#Programación orientada a objetos
+subj = Subject.find_by(code: 2016375)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
 
 
-        
 # Semestre 3
-
 sem = malla.semesters.find_by number: 3
 
-#Métodos cuantitativos en psicología
-subj = Subject.find_by(code: 2017860)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+#Introducción a la teoría de conjuntos
+subj = Subject.find_by(code: 2025819)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Métodos cualitativos en psicología
-subj = Subject.find_by(code: 2017861)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+#Calculo vectorial
+subj = Subject.find_by(code: 2015162)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Psicología del desarrollo
-subj = Subject.find_by(code: 2017906)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs
-#Epistemología de la psicología
-subj = Subject.find_by(code: 2017873)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs
-#Procesos básicos en psicología social
-subj = Subject.find_by(code: 2021669)
+#Elementos de computadores
+subj = Subject.find_by(code: 2016698)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
 
-        
+
 # Semestre 4
-
 sem = malla.semesters.find_by number: 4
 
-#Psicología Diferencial
-subj = Subject.find_by(code: 2017908)
+#Ecuaciones en diferencias finitas y sistemas dinámicos
+subj = Subject.find_by(code: 2026519)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Introducción al análisis real
+subj = Subject.find_by(code: 2015155)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Probabilidad
+subj = Subject.find_by(code: 2015178)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Estructuras de Datos
+subj = Subject.find_by(code: 2016699)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Perspectivas contemporáneas en psicología social
-subj = Subject.find_by(code: 2021667)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs
+
 
 # Semestre 5
-
 sem = malla.semesters.find_by number: 5
 
-#Psicología de lo Anormal
-subj = Subject.find_by(code: 2017905)
+#Algebra abstracta y computacional
+subj = Subject.find_by(code: 2026555)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Introducción al análisis combinatorio
+subj = Subject.find_by(code: 2027312)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Teoría de grafos
+subj = Subject.find_by(code: 2015184)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'B')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Arquitectura de computadores
+subj = Subject.find_by(code: 2016697)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
 
-        
-# Semestre 6
 
+# Semestre 6
 sem = malla.semesters.find_by number: 6
 
-#Evaluación psicológica
-subj = Subject.find_by(code: 2026113)
+#Análisis numérico
+subj = Subject.find_by(code: 2015150)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Psicología profesional
-subj = Subject.find_by(code: 2026114)
+#Teoría de Codificación
+subj = Subject.find_by(code: 2027313)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Electiva
+#Electiva 
 subj = Subject.find_by(code: 6666663)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
 career.career_has_subjects << chs
-sem.career_has_subjects << chs     
-        
+sem.career_has_subjects << chs
 
-        
+
 # Semestre 7
-
 sem = malla.semesters.find_by number: 7
-#Electiva
+
+#Sistemas operativos
+subj = Subject.find_by(code: 2016707)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Introducción a la teoría de la computación
+subj = Subject.find_by(code: 2015174)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Algoritmos
+subj = Subject.find_by(code: 2016696)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva 
+subj = Subject.find_by(code: 6666664)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva 
 subj = Subject.find_by(code: 6666663)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
 career.career_has_subjects << chs
-sem.career_has_subjects << chs  
-#Electiva
-subj = Subject.find_by(code: 6666663)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs  
-#Electiva
-subj = Subject.find_by(code: 6666663)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs  
+sem.career_has_subjects << chs
 
 
 
 # Semestre 8
-
 sem = malla.semesters.find_by number: 8
 
-#Electiva
+#Lógica computacional
+subj = Subject.find_by(code: 2027629)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva 
+subj = Subject.find_by(code: 6666664)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva 
 subj = Subject.find_by(code: 6666663)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
 career.career_has_subjects << chs
-sem.career_has_subjects << chs  
-#Electiva
-subj = Subject.find_by(code: 6666663)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs  
-#Electiva
-subj = Subject.find_by(code: 6666663)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs         
+sem.career_has_subjects << chs
 
+        
 # Semestre 9
-
 sem = malla.semesters.find_by number: 9
 
-#Práctica 1
-subj = Subject.find_by(code: 2017897)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs
-#Electiva
-subj = Subject.find_by(code: 6666663)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs  
-#Electiva
-subj = Subject.find_by(code: 6666663)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs  
-#Electiva
-subj = Subject.find_by(code: 6666663)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
-career.career_has_subjects << chs
-sem.career_has_subjects << chs  
-
-# Semestre 10
-
-sem = malla.semesters.find_by number: 10
-
 #Trabajo de Grado
-subj = Subject.find_by(code: 2017946)
+subj = Subject.find_by(code: 9992933)
 chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
-#Práctica 2
-subj = Subject.find_by(code: 2017898)
-chs = CareerHasSubject.create( :subject => subj, :typology => 'C')
+#Electiva
+subj = Subject.find_by(code: 6666664)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
+career.career_has_subjects << chs
+sem.career_has_subjects << chs
+#Electiva
+subj = Subject.find_by(code: 6666664)
+chs = CareerHasSubject.create( :subject => subj, :typology => 'L')
 career.career_has_subjects << chs
 sem.career_has_subjects << chs
 
-
-
-
+#-------------------------------------------------------------------------------------------
